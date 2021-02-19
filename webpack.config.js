6c316c05ac3a3bea96b87bba4ca56bdfbd9e1e70
krylovars <arsenjfr@gmail.com>
2021-02-19 17:25:17 +0300

    scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,36 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.[hash].js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
   plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].[hash].css",
+      chunkFilename: "[id].[hash].css",
+    }),
     new HtmlWebpackPlugin(),
     new CleanWebpackPlugin(),
     new CopyPlugin({
@@ -19,21 +38,5 @@ module.exports = {
         { from: path.resolve(__dirname, 'src/favicon.ico'), to: path.resolve(__dirname, 'dist') },
       ],
     }),
-    new MiniCssExtractPlugin(),
   ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-    ],
-  }
 };
