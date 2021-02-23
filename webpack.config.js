ddd6d9d7d07c8bd3a3ed5bc3bcf5e5f422261ea0
KrylovArseny <arsenjfr@gmail.com>
2021-02-23 16:06:08 +0300

    23

--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,6 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
       filename: "[name].[hash].css",
       chunkFilename: "[id].[hash].css",
     }),
